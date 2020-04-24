8c190887cd2cab82ded8ee177e207b6bfb623dc7
nicolai <mattrnicolai@gmail.com>
2020-04-23 22:01:54 -0400

    added framework of some functions. Added prompt function with all questions

--- index.js
+++ index.js
@@ -2,9 +2,70 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const markDown = require("./utils/generateMarkdown.js");
 
-const questions = [
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your github username?"
+        },
 
-];
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+
+        {
+            type: "input",
+            name: "URL",
+            message: "What is the URL to your project?"
+        },
+
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is the name of your project?"
+        },
+
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a short description of your project:"
+        },
+
+        {
+            // What type should this be
+            type: "",
+            name: "license",
+            message: "What kind of license will be needed to use this project?"
+        },
+
+        {
+            type: "input",
+            name: "dependencies",
+            message: "What command should be run to install dependencies?"
+        },
+
+        {
+            type: "input",
+            name: "tests",
+            message: "What command should be run to run tests?"
+        },
+
+        {
+            type: "input",
+            name: "usage",
+            message: "What does your user need to know about using this repo?"
+        },
+
+        {
+            type: "input",
+            name: "contributing",
+            message: "What does your user need to know to be able to contribute to this repo?"
+        },
+    ])
+};
 
 function writeToFile(fileName, data) {
 }
